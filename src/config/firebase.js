9f882974288a91905b57fbccff77f6d71c04722f
Sebastien Niang <sebastien.niang@gmail.com>
2022-11-15 12:43:21 +0100

    Firebase project setup

---
+++ src/config/firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.FB_API_KEY,
+  authDomain: process.env.FB_AUTH_DOMAIN,
+  databaseURL: process.env.FB_DATABASE_URL,
+  projectId: process.env.FB_PROJECT_ID,
+  storageBucket: process.env.FB_STORE_BUCKET,
+  messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+  appId: process.env.FB_APP_ID,
+  measurementId: process.env.FB_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+export const firebaseAuth = firebase.auth;
+export const db = firebase
+  .firestore()
+  .settings({ timestampsInSnapshots: true });
+//export const analytics = getAnalytics(app);
