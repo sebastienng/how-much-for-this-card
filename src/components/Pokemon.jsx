0bf592d02db90497f14d619c20589cdf6f2f1d62
Sebastien Niang <sebastien.niang@gmail.com>
2022-11-20 12:29:38 +0100

    hosting

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Pokemon = () => {
-  return <div>Pokemon</div>;
+  return (
+    <>
+      <div>Pokemon</div>
+      {/* <label>
+        <i className="bottom-icon material-icons">camera_alt</i>
+        <FileUploader
+          hidden
+          accept="image/*"
+          storageRef={firebase.storage().ref("images")}
+          onUploadStart={this.handleUploadStart}
+          onUploadError={this.handleUploadError}
+          onUploadSuccess={this.handleUploadSuccess}
+          onProgress={this.handleProgress}
+        />
+      </label> */}
+    </>
+  );
 };
 
 export default Pokemon;
